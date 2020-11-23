3c2ba2bc905361a67a2ec8262a5bf66468a1e7c5
luisfelipegs <luisfelipe14@live.com.mx>
2020-11-23 11:09:54 -0600

    funciones javascript

---
+++ funciones.js
@@ -0,0 +1,21 @@
+function Usuario (nombre, correo , edad, comentario) {
+    this.correonombre=nombre;
+    this.correo=correo;
+    this.edad=edad;
+    this.comentario=comentario;
+}
+
+const usiario1 = new
+usuario ("Luis", "luisfelipe14@live.com.mx", "32", "extreme biker"
+);
+const usuario2 = new
+usuario ("felipe", "felipe111@hotmail.com", "22", "abogado mercantil");
+const usuario3 = new usuario ("he-man", "hman@hotmail.com", "33", "profensional party dancer");
+
+const datos = [usuiario1, usuario2, usuario3]
+
+console.log(`********** Datos de Usuario ${dato.nombre} **********`)
+        for(propiedad in dato){
+             console.log(dato[propiedad])
+        }
+   })
